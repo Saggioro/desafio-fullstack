aef418c9afc153f532034b7caa681921e8f1d4cf
Saggioro <diegosaggiorobrito@gmail.com>
2021-07-17 15:17:47 -0300

    feat: adicionado entidade pessoa

---
+++ src/modules/pessoas/infra/typeorm/entities/Pessoa.ts
@@ -0,0 +1,42 @@
+import {
+    Entity,
+    Column,
+    PrimaryGeneratedColumn,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from "typeorm";
+
+@Entity("pessoas")
+class Pessoa {
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column()
+    nome: string;
+
+    @Column()
+    sexo: "Feminino" | "Masculino";
+
+    @Column()
+    nascimento: Date;
+
+    @Column()
+    naturalidade: string;
+
+    @Column()
+    nacionalidade: string;
+
+    @Column()
+    cpf: string;
+
+    @Column()
+    email?: string;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+export default Pessoa;

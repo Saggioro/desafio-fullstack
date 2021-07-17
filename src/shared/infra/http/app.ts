bf1f3b01b757b0677d1015f2e397a4bc0396cd30
Saggioro <diegosaggiorobrito@gmail.com>
2021-07-17 18:09:14 -0300

    feat: adicionado error handler global

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,10 +2,11 @@ import "reflect-metadata";
 import "dotenv/config";
 import "express-async-errors";
 import cors from "cors";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
 
 import createConnection from "@shared/infra/typeorm";
 
+import AppError from "../../errors/AppError";
 import routes from "./routes";
 import "@shared/container";
 
@@ -16,5 +17,20 @@ app.use(express.json());
 app.use(cors());
 
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        return response
+            .status(err.statusCode)
+            .json({ status: "error", message: err.message });
+    }
+
+    console.error(err);
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server error",
+    });
+});
 
 export { app };

b0cb2d4ec994b3df77457d59babeec28a98618a3
Saggioro <diegosaggiorobrito@gmail.com>
2021-07-17 14:38:12 -0300

    feat: adicionado providers ao container

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,6 +2,8 @@ import UsuariosRepository from "@modules/usuarios/infra/typeorm/repositories/Usu
 import IUsuariosRepository from "@modules/usuarios/repositories/IUsuariosRepository";
 import { container } from "tsyringe";
 
+import "./providers";
+
 container.registerSingleton<IUsuariosRepository>(
     "UsuariosRepository",
     UsuariosRepository

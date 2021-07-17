01b87ef133856211ec51fd4203243b7bc6f5b85a
Saggioro <diegosaggiorobrito@gmail.com>
2021-07-17 15:14:45 -0300

    feat: adicionado DTO para criação de pessoas

---
+++ src/modules/pessoas/dtos/ICreatePessoaDTO.ts
@@ -0,0 +1,9 @@
+export default interface ICreatePessoaDTO {
+    nome: string;
+    sexo: "Feminino" | "Masculino";
+    nascimento: Date;
+    naturalidade: string;
+    nacionalidade: string;
+    cpf: string;
+    email?: string;
+}

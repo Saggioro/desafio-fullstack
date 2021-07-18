6efe153ce7cbb580d302950388f8523b4c199d0e
Saggioro <diegosaggiorobrito@gmail.com>
2021-07-18 14:27:24 -0300

    feat: adicionado verificação de cep ao brazilvaluesprovider

--- src/shared/container/providers/BrazilValuesProvider/implementations/BrazilianValuesProvider.ts
+++ src/shared/container/providers/BrazilValuesProvider/implementations/BrazilianValuesProvider.ts
@@ -1,10 +1,13 @@
-import { isCPF } from "brazilian-values";
+import { isCEP, isCPF } from "brazilian-values";
 
 import IBrazilValuesProvider from "../models/IBrazilValuesProvider";
 
 class BrazilianValuesProvider implements IBrazilValuesProvider {
     public validateCpf(payload: string): boolean {
         return isCPF(payload);
+    }
+    public validateCep(payload: string): boolean {
+        return isCEP(payload);
     }
 }
 
--- src/shared/container/providers/BrazilValuesProvider/models/IBrazilValuesProvider.ts
+++ src/shared/container/providers/BrazilValuesProvider/models/IBrazilValuesProvider.ts
@@ -1,3 +1,4 @@
 export default interface IBrazilValuesProvider {
     validateCpf(payload: string): boolean;
+    validateCep(payload: string): boolean;
 }
